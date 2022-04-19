a6df548a22f622de5f2e3802fc514d5f3e69592e
Byenkya <byenkyajoshua1997@gmail.com>
2022-04-19 04:19:21 -0700

    changes to print_binary

--- print_binary.c
+++ print_binary.c
@@ -5,41 +5,12 @@
 * @printed: hold the number of characters printed
 */
 
-int print_binary(va_list list)
+void print_binary(unsigned int n, unsigned int *printed)
 {
-	unsigned int num;
-
-	int i, len;
-
-	char *str;
-
-	char *rev_str;
-
-	num = va_arg(list, unsigned int);
-
-	if (num == 0)
-		return (_write_char('0'));
-
-	if (num < 1)
-	return (-1);
-	len = base_len(num, 2);
-	str = malloc(sizeof(char)* len + 1);
-	if (str == NULL)
-		return (-1);
-	for (i = 0; num > 0; i++)
+	if (n > 1)
 	{
-		if (num % 2 == 0)
-			str[i] = '0';
-		else
-			str[i] = '1';
-		num = num / 2;
+		*printed += 1;
+		print_binary(n >> 1, printed);
 	}
-	str[i] = '\0';
-	rev_str = rev_string(str);
-	if (rev_str == NULL)
-		return (-1);
-	write_base(rev_str);
-	free(str);
-	free(rev_str);
-	return (len);
+	_putchar((n & 1) + '0');
 }
